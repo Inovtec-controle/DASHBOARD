6250a79d59cafc6e0310cbac53eca49e64ceee4e
Inovtec <labarthe.alain@gmail.com>
2026-08-06 14:07:36 +0200

    Centralize Firebase web configuration

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.INOVTEC_FIREBASE_CONFIG = Object.freeze({
+  apiKey: "AIzaSyCd_A1V-CRWGxbEmGFDadNFbGqXLocBDPw",
+  authDomain: "inovtec-chantiers.firebaseapp.com",
+  projectId: "inovtec-chantiers",
+  storageBucket: "inovtec-chantiers.firebasestorage.app",
+  messagingSenderId: "313162345276",
+  appId: "1:313162345276:web:1a270f797dd736a4060c39"
+});
