8950b4ee4178a68d8b41bc6cabc99408a5cbb639
Inovtec <labarthe.alain@gmail.com>
2026-08-13 15:01:18 +0200

    Renforce la redirection Firebase Discipline

--- discipline-firestore-bridge.js
+++ discipline-firestore-bridge.js
@@ -2,52 +2,46 @@
   "use strict";
   if(!window.firebase || typeof firebase.firestore !== "function") return;
 
-  var originalFirestore = firebase.firestore;
-
   function hiddenDoc(ref, type){
     return {
-      get: function(){ return ref.get(); },
-      onSnapshot: function(){ return ref.onSnapshot.apply(ref, arguments); },
-      set: function(data, options){
-        var payload = Object.assign({_hidden:true, _type:type}, data || {});
-        return ref.set(payload, options);
-      },
-      update: function(){ return ref.update.apply(ref, arguments); },
-      delete: function(){ return ref.delete(); }
+      get:function(){return ref.get();},
+      onSnapshot:function(){return ref.onSnapshot.apply(ref,arguments);},
+      set:function(data,options){return ref.set(Object.assign({_hidden:true,_type:type},data||{}),options);},
+      update:function(){return ref.update.apply(ref,arguments);},
+      delete:function(){return ref.delete();}
     };
   }
 
-  function wrappedFirestore(){
-    var realDb = originalFirestore.apply(firebase, arguments);
-    return new Proxy(realDb, {
-      get: function(target, prop){
-        if(prop === "collection"){
-          return function(name){
-            if(name === "discipline"){
-              return {
-                doc: function(uid){
-                  return hiddenDoc(target.collection("chantiers").doc("__discipline_data__" + uid), "disciplineData");
-                }
-              };
-            }
-            if(name === "kanban"){
-              return {
-                doc: function(uid){
-                  return hiddenDoc(target.collection("chantiers").doc("__discipline_backup__" + uid), "disciplineBackup");
-                }
-              };
-            }
-            return target.collection(name);
-          };
-        }
-        var value = target[prop];
-        return typeof value === "function" ? value.bind(target) : value;
+  function redirectedCollection(db, originalCollection, name){
+    if(name!=="discipline" && name!=="kanban") return originalCollection.call(db,name);
+    var real=originalCollection.call(db,"chantiers");
+    return {
+      doc:function(uid){
+        var prefix=name==="discipline"?"__discipline_data__":"__discipline_backup__";
+        var type=name==="discipline"?"disciplineData":"disciplineBackup";
+        return hiddenDoc(real.doc(prefix+uid),type);
       }
-    });
+    };
   }
 
-  Object.getOwnPropertyNames(originalFirestore).forEach(function(key){
-    try{ wrappedFirestore[key] = originalFirestore[key]; }catch(e){}
-  });
-  firebase.firestore = wrappedFirestore;
+  var FirestoreClass=firebase.firestore.Firestore;
+  if(FirestoreClass && FirestoreClass.prototype && typeof FirestoreClass.prototype.collection==="function"){
+    var originalCollection=FirestoreClass.prototype.collection;
+    if(!originalCollection.__inovtecDisciplineBridge){
+      var patched=function(name){return redirectedCollection(this,originalCollection,name);};
+      patched.__inovtecDisciplineBridge=true;
+      FirestoreClass.prototype.collection=patched;
+    }
+    return;
+  }
+
+  var originalFirestore=firebase.firestore;
+  function wrappedFirestore(){
+    var realDb=originalFirestore.apply(firebase,arguments);
+    var originalCollection=realDb.collection;
+    realDb.collection=function(name){return redirectedCollection(realDb,originalCollection,name);};
+    return realDb;
+  }
+  Object.getOwnPropertyNames(originalFirestore).forEach(function(key){try{wrappedFirestore[key]=originalFirestore[key];}catch(e){}});
+  try{firebase.firestore=wrappedFirestore;}catch(e){console.warn("Pont Firebase Discipline non installé",e);}
 })();
