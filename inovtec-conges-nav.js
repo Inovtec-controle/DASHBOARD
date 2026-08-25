3eeea49f22a06a2682e73ea8c1487366ae2a09e7
Inovtec <labarthe.alain@gmail.com>
2026-08-25 17:28:58 +0200

    Charger le correctif de liste complète des congés

--- inovtec-conges-nav.js
+++ inovtec-conges-nav.js
@@ -1,1 +1,18 @@
-(()=>{"use strict";if(document.querySelector('script[data-iv-nav-order="1"]'))return;const s=document.createElement("script");s.src="inovtec-navigation-order.js?v=20260815-2";s.dataset.ivNavOrder="1";s.async=false;document.head.appendChild(s)})();
+(()=>{
+"use strict";
+if(!document.querySelector('script[data-iv-nav-order="1"]')){
+  const s=document.createElement("script");
+  s.src="inovtec-navigation-order.js?v=20260815-2";
+  s.dataset.ivNavOrder="1";
+  s.async=false;
+  document.head.appendChild(s);
+}
+const mode=(new URLSearchParams(location.search).get("mode")||"").toLowerCase();
+if(mode==="conges"&&!document.querySelector('script[data-iv-conges-full-list="1"]')){
+  const s=document.createElement("script");
+  s.src="inovtec-conges-full-list.js?v=20260825-1";
+  s.dataset.ivCongesFullList="1";
+  s.async=false;
+  document.head.appendChild(s);
+}
+})();
