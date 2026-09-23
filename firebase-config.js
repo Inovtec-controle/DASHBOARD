944e72b7eaffe7aa5d90910d6b371e0699ac02da
Inovtec <labarthe.alain@gmail.com>
2026-09-23 14:57:23 +0200

    Force le correctif de session non bloquante du Planning

--- firebase-config.js
+++ firebase-config.js
@@ -68,7 +68,7 @@ window.INOVTEC_FIREBASE_CONFIG = Object.freeze({
         }
         if (!doc.querySelector('script[data-iv-stable-ui="1"]') && !p.__INOVTEC_UI_STABILITY_V1__) {
           const script = doc.createElement('script');
-          script.src = 'inovtec-ui-stability.js?v=20260917-ui-stable2';
+          script.src = 'inovtec-ui-stability.js?v=20260923-planning-clickfix1';
           script.dataset.ivStableUi = '1';
           script.async = false;
           (doc.head || doc.documentElement).appendChild(script);
@@ -92,7 +92,7 @@ window.INOVTEC_FIREBASE_CONFIG = Object.freeze({
     }
     if (!document.querySelector('script[data-iv-stable-ui="1"]') && !window.__INOVTEC_UI_STABILITY_V1__) {
       const script = document.createElement("script");
-      script.src = "inovtec-ui-stability.js?v=20260917-ui-stable2";
+      script.src = "inovtec-ui-stability.js?v=20260923-planning-clickfix1";
       script.dataset.ivStableUi = "1";
       script.async = false;
       (document.head || document.documentElement).appendChild(script);
